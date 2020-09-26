4521bea4a4661345a5b14e17a16c3672209d7a10
Sydney <Sydney@DESKTOP-VVI54IM>
2020-09-26 13:42:56 -0300

    Array

---
+++ Arrays/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace arreys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnvetor = new System.Windows.Forms.Button();
+            this.btntextos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnmulti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnvetor
+            // 
+            this.btnvetor.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnvetor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnvetor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvetor.Location = new System.Drawing.Point(26, 48);
+            this.btnvetor.Name = "btnvetor";
+            this.btnvetor.Size = new System.Drawing.Size(103, 71);
+            this.btnvetor.TabIndex = 0;
+            this.btnvetor.Text = "teste  vetor";
+            this.btnvetor.UseVisualStyleBackColor = false;
+            this.btnvetor.Click += new System.EventHandler(this.btnvetor_Click);
+            // 
+            // btntextos
+            // 
+            this.btntextos.BackColor = System.Drawing.Color.DarkOrange;
+            this.btntextos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btntextos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntextos.Location = new System.Drawing.Point(157, 48);
+            this.btntextos.Name = "btntextos";
+            this.btntextos.Size = new System.Drawing.Size(114, 71);
+            this.btntextos.TabIndex = 0;
+            this.btntextos.Text = "vetor textos";
+            this.btntextos.UseVisualStyleBackColor = false;
+            this.btntextos.Click += new System.EventHandler(this.btntextos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(121, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Array";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Array Multidimencional";
+            // 
+            // btnmulti
+            // 
+            this.btnmulti.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnmulti.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnmulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmulti.Location = new System.Drawing.Point(99, 192);
+            this.btnmulti.Name = "btnmulti";
+            this.btnmulti.Size = new System.Drawing.Size(112, 58);
+            this.btnmulti.TabIndex = 3;
+            this.btnmulti.Text = "arrey multi";
+            this.btnmulti.UseVisualStyleBackColor = false;
+            this.btnmulti.Click += new System.EventHandler(this.btnmulti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(321, 284);
+            this.Controls.Add(this.btnmulti);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btntextos);
+            this.Controls.Add(this.btnvetor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnvetor;
+        private System.Windows.Forms.Button btntextos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnmulti;
+    }
+}
+
---
+++ Arrays/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arreys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnvetor_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[4];
+            vetor[0] = 7; vetor[1] = 3;
+            vetor[2] = 5; vetor[3] = 1;
+            int valor = vetor[1];
+            System.Array.Sort(vetor);
+            for (int index = 0; index < 4; index++)
+                MessageBox.Show(vetor[index].ToString(), "Array");
+            
+            /*Declarar e instanciar vetor Atribuição de valores de acordo com cada índice Declara variável "valor" para acessar o
+             * vetor System.Array.Sort - Esta classe ordena o vetor Comando for- Percorre o vetor em cada índice. Condição - Para index menor que 4 (de 0 a 3),
+             * ele exibe uma msg com o vetor na MessageBox. */
+        }
+
+        private void btntextos_Click(object sender, EventArgs e)
+        {
+            string[] semana = new string[7];
+            semana[0] = "Domingo";
+            semana[1] = "Segunda-Feira";
+            semana[2] = "Terça-Feira";
+            semana[3] = "Quarta-Feira";
+            semana[4] = "Quinta-Feira";
+            semana[5] = "Sexta-Feira";
+            semana[6] = "Sábado";
+            string valor = semana[1];
+
+            for (int index = 0; index < 7; index++)
+
+                MessageBox.Show(semana[index].ToString(), "Array");
+
+            /*Declarar e instanciar vetor Atribuição de valores de acordo com cada índice,
+             * Aqui no caso são os dias da semana (string) Declara variável "valor" para acessar o
+             * vetor Por se tratar de string não será necessário ordenar Comando for- Percorre o vetor 
+             * em cada índice. Condição Para index menor que 7 (de 0 a 6), ele exibe uma msg com o vetor na MessageBox, ou seja cada dia da semana. */
+        }
+
+        private void btnmulti_Click(object sender, EventArgs e)
+        {
+            double[,] notas = new double[2, 4];
+            notas[0, 0] = 8.0;
+            notas[0, 1] = 7.5;
+            notas[0, 2] = 9.0;
+            notas[0, 3] = 8.5;
+            notas[1, 0] = 9.5;
+            notas[1, 1] = 8.5;
+            notas[1, 2] = 8.0;
+            notas[1, 3] = 9.0;
+
+            MessageBox.Show(notas[0, 1].ToString(), "Array");
+            MessageBox.Show(notas[1, 1].ToString(), "Array");
+
+
+            /*Declarar e instanciar array, indicando qtde linhas e colunas Atribuição de valores
+             * de acordo com cada índice, Exibe na Messagebox a devida nota de acordo com os índices escolhidos. */
+        }
+    }
+}
